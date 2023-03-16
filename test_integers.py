de02cf548649cadc5ebb0fd1ab76f5faf83621a2
Sasoriweb <114301099+Sasoriweb@users.noreply.github.com>
2023-03-16 10:32:10 -0300

    Update test_integers.py

--- test_integers.py
+++ test_integers.py
@@ -4,26 +4,27 @@
 
 
 def test_integers_basic() -> None:
-    # vamos a hacer primero una asignación de variable
+    
+    numero = 3 # vamos a hacer primero una asignación de variable
     assert numero == 3
 
     # luego vamos a cambiar el valor de esta variable
-
+    numero = 5
     assert numero == 5
 
     # luego vamos a operar con otro integer con una suma
-
+    suma = numero + 5
     assert suma == 10
 
     # ahora intentemos hacer una división
-
+    division = suma / numero
     assert division == 2
 
 def test_integers_string() -> None:
     # partimos con un integer y transformemoslo a string
-
+    str_numero = "3"
     assert str_numero == "3"
 
     # luego veamos si podemos transformar ese str a int
-
+    numero = int(str_numero)
     assert numero == 3
